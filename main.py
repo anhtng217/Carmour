55baa42ec048c8a2388341dde72faf8af6892f1f
Anh Nguyen <anhtng217@gmail.com>
2021-03-08 08:40:41 -0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# main.py
+# import the necessary packages
+from flask import Flask, render_template, Response, request
+from camera import VideoCamera
+import time
+import threading
+import os
+
+pi_camera = VideoCamera(flip=False) # Flip the camera if needed
+
+# App globals 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html') # Simple web template to stream to
+
+#get camera frame
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(pi_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', debug=False)
+    
+
+
---
+++ recording.py
@@ -0,0 +1,64 @@
+# recording.py
+# import the necessary packages
+import picamera
+import os
+import datetime as dt
+from subprocess import call
+from Google import Create_Service
+from googleapiclient.http import MediaFileUpload
+
+def piRecord():  
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1920, 1080)     # set video resolution
+        camera.annotate_background = picamera.Color('black')
+        camera.annotate_text = dt.datetime.now().strftime('%Y/%m/%d-%H:%M:%S')
+        camera.start_recording('/home/pi/Desktop/footage.h264') # record video
+    
+        # Display timestamps in the video
+        start = dt.datetime.now()
+        while (dt.datetime.now() - start).seconds <20:
+            camera.annotate_text = dt.datetime.now().strftime('%Y/%m/%d-%H:%M:%S')
+            camera.wait_recording(0.2)            
+        camera.stop_recording()                
+        
+        print('Footage recored and saved. Begin to upload to database')
+            # convert file to mp4 file type
+            #convertVid = "MP4Box -add video.h264 video.mp4"  
+            #call ([convertVid], shell=True)
+        
+        #os.remove("video.h264")   # remove extension .h264
+    
+    #upload to Youtube
+    CLIENT_SECRET_FILE = 'Client_Secret1.json'
+    API_NAME = 'youtube'
+    API_VERSION = 'v3'
+    SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+    
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+    
+    request_body = {
+        'snippet': {
+            'title': 'Camera Footage',
+            'description': 'Recorded footage from cameras'
+            },
+        'status': {
+            'privacyStatus': 'private',
+            'selfDeclaredMadeForKids': False,
+            },
+        'notifySubscribers': False
+        }
+    
+    mediaFile = MediaFileUpload('/home/pi/Desktop/footage.h264')
+    
+    response_upload = service.videos().insert(
+        part='snippet,status',
+        body=request_body,
+        media_body=mediaFile
+    ).execute()
+    
+    service.thumbnails().set(
+        videoId = response_upload.get('id'),
+        media_body = MediaFileUpload('Carmour Logo.jpg')).execute()
+    
+    print('Footage successfully uploaded to database!')
+
